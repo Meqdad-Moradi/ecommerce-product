28fcfa8bd3063053c2b682d799c31e61de3769c9
meqdad <meqdad.moradi@outlook.com>
2021-10-13 23:19:35 +0200

    some changes to card

--- main.js
+++ main.js
@@ -108,8 +108,9 @@ close.addEventListener('click', e => {
 })
 
 
-//////////////////////////////////
-// checkout the product
+/*=========================
+    checkout the product
+=========================*/
 
 // create cart item
 const productItem = [{
@@ -120,31 +121,34 @@ const productItem = [{
 }]
 
 const cart = document.querySelector('.cart')
+const cartContent = document.querySelector('.cart-content')
 const cartBody = document.querySelector('.cart-body')
 const addToCartBtn = document.querySelector('#add-to-cart')
 const decrementBtn = document.querySelector('#decrement')
 const quantityBtn = document.querySelector('#quantity')
 const incrementBtn = document.querySelector('#increment')
 const cartInfo = document.querySelector('.cart-info')
+const avatar = document.querySelector('.avatar')
 
 let quantity = 1
 
 
-// create cart container
-cart.addEventListener('click', e => {
-    const cartContent = document.querySelector('.cart-content')
-    cartContent.classList.toggle('active')
+// display cart container
+const toggleCartContent = () => {
+    if (!cartContent.classList.contains('active')) {
+        cartContent.classList.add('active')
+        avatar.style.borderColor = 'hsl(26, 100%, 55%)'
+    } else {
+        avatar.style.borderColor = 'transparent'
+        cartContent.classList.remove('active')
+    }
+}
+cart.addEventListener('click', toggleCartContent)
+avatar.addEventListener('click', toggleCartContent)
 
-    cartContent.addEventListener('click', e => {
-        if (e.target === e.currentTarget) {
-            return
-        } else {
-            e.currentTarget.classList.remove('active')
-        }
-    })
-})
+// 
 
-
+// decrement the item's amount
 decrementBtn.addEventListener('click', e => {
     quantity--
     if (quantity <= 0) {
@@ -155,13 +159,13 @@ decrementBtn.addEventListener('click', e => {
     quantityBtn.textContent = quantity
 })
 
-
+// increment the item's amount
 incrementBtn.addEventListener("click", e => {
     quantity++
     quantityBtn.textContent = quantity
 })
 
-
+// add the item to cart 
 addToCartBtn.addEventListener('click', e => {
     cartInfo.style.display = 'none'
     cartBody.style.display = 'block'
@@ -175,14 +179,14 @@ addToCartBtn.addEventListener('click', e => {
                 <div class="cart-item-details">
                   <p class="product-name">${productItem[0].pName}</p>
                   <div class="cart-prices">
-                    <span class="price">$${productItem[0].price}</span>
+                    <span class="price">$${productItem[0].price}.00</span>
                     <span class="calc">x <span class="quantity">${quantity}</span></span>
                     <span class="total-price">$${productItem[0].price * quantity}.00</span>
                   </div>
                 </div>
 
                 <div class="remove-product" title="Delete item">
-                  <img src="./images/icon-delete.svg" alt="delete">
+                <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
                 </div>
               </div>
               
@@ -191,7 +195,14 @@ addToCartBtn.addEventListener('click', e => {
     const deleteBtn = document.querySelector('.remove-product')
     deleteBtn.addEventListener('click', () => {
         cartBody.style.display = 'grid'
-    cartBody.style.height = '200px'
+        cartBody.style.height = '200px'
         cartBody.innerHTML = '<p class="cart-info">your cart is empty!</p>'
     })
+})
+
+// remove the cart content
+cartContent.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) {
+        cartContent.classList.remove('active')
+    }
 })
